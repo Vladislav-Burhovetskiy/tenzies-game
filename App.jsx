90b9e2aa040a2d27595b3a993d038f8cbc75b568
Vladislav Burhovetskiy <mylivladislav@gmail.com>
2024-01-26 17:29:21 +0200

    * change dice array to object

--- App.jsx
+++ App.jsx
@@ -1,39 +1,36 @@
-import React from "react"
-import Die from "./Die.jsx"
-
+import React from "react";
+import Die from "./Die.jsx";
+import { nanoid } from "nanoid";
 
 export default function App() {
-/**
- * Challenge: Update the array of numbers in state to be
- * an array of objects instead. Each object should look like:
- * { value: <random number>, isHeld: false }
- * 
- * Making this change will break parts of our code, so make
- * sure to update things so we're back to a working state
- */
+  const [dice, setDice] = React.useState(allNewDice());
 
-    const [dice, setDice] = React.useState(allNewDice())
-    
-    function allNewDice() {
-        const newDice = []
-        for (let i = 0; i < 10; i++) {
-            newDice.push(Math.ceil(Math.random() * 6))
-        }
-        return newDice
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
     }
-    
-    function rollDice() {
-        setDice(allNewDice())
-    }
-    
-    const diceElements = dice.map(die => <Die value={die} />)
-    
-    return (
-        <main>
-            <div className="dice-container">
-                {diceElements}
-            </div>
-            <button className="roll-dice" onClick={rollDice}>Roll</button>
-        </main>
-    )
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die key={die.id} value={die.value} />
+  ));
+
+  return (
+    <main>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
+    </main>
+  );
 }
